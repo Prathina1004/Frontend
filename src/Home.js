a937c276cbfd7584599359788a8d6673204a6320
Prathina1004 <prathinaveeraselvam@gmail.com>
2024-03-11 04:07:38 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://imgmediagumlet.lbb.in/media/2022/01/61e7be0d8db24021a0825254_1642577421930.jpg?fm=webp&w=750&h=500&dpr=1" alt="Welcome picture" />
+    </div>
+  )
+}
